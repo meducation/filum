61b3207d21a4017fc4c2ec8d90a00d035602bdeb
MalcyL <malcolm@landonsonline.me.uk>
2013-11-20 11:18:09 +0000

    Fixed log line details.

--- lib/filum/logger.rb
+++ lib/filum/logger.rb
@@ -7,28 +7,23 @@ module Filum
     end
 
     def info(str)
-      super(format("INFO", str))
+      super("#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] INFO | #{str}")
     end
 
     def fatal(str)
-      super(format("FATAL", str))
+      super("#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] FATAL | #{str}")
     end
 
     def error(str)
-      super(format("ERROR", str))
+      super("#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] ERROR | #{str}")
     end
 
     def warn(str)
-      super(format("WARN", str))
+      super("#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] WARN | #{str}")
     end
 
     def debug(str)
-      super(format("DEBUG", str))
-    end
-
-    private
-    def fromat(level, str)
-      "#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] #{level} | #{str}"
+      super("#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] DEBUG | #{str}")
     end
   end
 end
