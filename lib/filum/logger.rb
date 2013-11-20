aa38dfe75be43184931768d723a97887d28ce880
MalcyL <malcolm@landonsonline.me.uk>
2013-11-20 11:11:59 +0000

    Added thread-id before thread id in log line.

--- lib/filum/logger.rb
+++ lib/filum/logger.rb
@@ -7,23 +7,23 @@ module Filum
     end
 
     def info(str)
-      super("#{DateTime.now} #{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] INFO | #{str}")
+      super("#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] INFO | #{str}")
     end
 
     def fatal(str)
-      super("#{DateTime.now} #{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] FATAL | #{str}")
+      super("#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] FATAL | #{str}")
     end
 
     def error(str)
-      super("#{DateTime.now} #{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] ERROR | #{str}")
+      super("#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] ERROR | #{str}")
     end
 
     def warn(str)
-      super("#{DateTime.now} #{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] WARN | #{str}")
+      super("#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] WARN | #{str}")
     end
 
     def debug(str)
-      super("#{DateTime.now} #{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] DEBUG | #{str}")
+      super("#{DateTime.now} thread_id-#{Thread.current.object_id} #{caller[0]} [#{Thread.current[:context_id]}] DEBUG | #{str}")
     end
   end
 end
